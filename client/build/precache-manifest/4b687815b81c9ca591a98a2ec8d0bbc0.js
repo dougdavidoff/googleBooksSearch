ebe7d9cebd545ed7110759eb575a2bb5b3b1f2f3
Doug Davidoff <douglass.davidoff@gmail.com>
2021-01-31 10:19:50 -0500

    adding precache manifest files

---
+++ client/build/precache-manifest/4b687815b81c9ca591a98a2ec8d0bbc0.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+    {
+        "revision": "229c360febb4351a89df",
+        "url": "/static/js/runtime-main.229c360f.js"
+    },
+    {
+        "revision": "3a249ec9c0db7b075877",
+        "url": "/static/js/main.3a249ec9.chunk.js"
+    },
+    {
+        "revision": "1d0562a8905232dc7d373927807ebbad",
+        "url": "/index.html"
+    }
+];
