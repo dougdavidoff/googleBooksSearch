ac8241f10ea1fbafa00eda428837df220207d123
Doug Davidoff <douglass.davidoff@gmail.com>
2021-01-31 12:27:45 -0500

    Adding App.js

---
+++ client/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowseRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import Saved from "./pages/Saved";
+import Search from "./pages/Search";
+
+function App() {
+    return (
+        <Router>
+            <div>
+                <NavTabs />
+                <Route exact path="/" component={Search} />
+                <Route exact path="/search" component={Search} />
+                <Route exact path="/saved" component={Saved} />
+            </div>
+        </Router>
+    );
+}
+
+export default App;
