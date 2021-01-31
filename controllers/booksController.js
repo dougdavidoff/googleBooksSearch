67b2a8dddef5ff7757be980e1da2feed5dc19cb8
Doug Davidoff <douglass.davidoff@gmail.com>
2021-01-31 12:41:19 -0500

    Adding booksController.js

---
+++ controllers/booksController.js
@@ -0,0 +1,17 @@
+const db = require("../models";
+
+module.exports = {
+    findAll: function(req, res) {
+        db.Book
+            .find(req,query)
+            .then(dbModel => res.jsojn(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    create: function(req, res) {
+        console.log("Hitting the create function in controller");
+        db.Book
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    }
+}
